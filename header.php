6aa135c8e6dafc615f9463adfdc92383a65911a3
vincentbasset <vincent.basset@uha.fr>
2017-05-29 23:29:14 +0200

    Update header.php
    j'ai séparé la page en 3 colonnes mais a mon avis a partir de la ligne 23 (<div class="centre">)le reste ne vas pas la (puisqu'il sera spécifique à la page en question)

--- header.php
+++ header.php
@@ -15,13 +15,11 @@
 			<h1><a href="index.php"><img src="image/logo.jpg" title="Le Cercle des Ingénieurs de l'ENSISA" alt="C.I.D.E." width="200px" height="150px" /></a></h1>
 			<p><a href="inscription.php">Inscription</a></p>
 		</header>
+		
 		<div class="gauche">
-			<nav>
-				<a href="index.php">Accueil</a>
-				<a href="groupe.php">Mes Groupes</a>
-				<a href="profil.php">Mon profil</a>
-			</nav>
+			<?php include("navigation.php"); ?>
 		</div>
+		
 		<div class="centre">
 			<p>
 				test test test test test test test test test test test test test
@@ -40,6 +38,7 @@
 				test test test test test test test test test test test test test
 			</p>
 		</div>
+		
 		<div class="droit">
 			<p>
 				test test test test test test test test test test test test test

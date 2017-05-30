2c417c3f23aa76883298a433d0b54b3727a59b0a
fannyringler <fanny.ringler@uha.fr>
2017-05-30 09:58:25 +0200

    Add files via upload

---
+++ cide/index.php
@@ -0,0 +1,160 @@
+<?php
+	include("header.php");
+?>
+			<div class="centre">
+			<p>
+				<span>
+				<img src="image/profil.jpg" title="Mr TrucMuche" alt="connard" width="50px" height="50px" />
+				<a href="profil.php">Mr TrucMuche</a>      <!--lien vers le profil de la personne-->
+				</span></br>
+				test test test test test test test test test test test test test
+				test test test test test test #1AINFO test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+			
+			<p>
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+			
+			<p>
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+			<p>
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+			<p>
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+			<p>
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+			<p>
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+			<p>
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+		</div>
+		
+		<div class="droit">
+			<p>
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+				test test test test test test test test test test test test test
+			</p>
+		</div>
+	</body>
+</html>
---
+++ cide/navigation.php
@@ -0,0 +1,5 @@
+<nav>
+	<a href="index.php">Accueil</a>
+	<a href="groupe.php">Mes Groupes</a>
+	<a href="profil.php">Mon profil</a>
+</nav>

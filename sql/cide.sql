28eb36a91834324545b9d0f0f394a27047dad047
Getsuren <vernay-jacques@hotmail.fr>
2017-05-30 17:14:29 +0200

    modification cide.sql
    changement des tailles de variables

--- sql/cide.sql
+++ sql/cide.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 30 Mai 2017 à 16:04
+-- Généré le :  Mar 30 Mai 2017 à 17:06
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.19
 
@@ -40,7 +40,7 @@ CREATE TABLE `affiche` (
 CREATE TABLE `appartient` (
   `idUtil` int(11) NOT NULL,
   `idGroupe` int(11) NOT NULL,
-  `droit` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'membre'
+  `droit` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'membre'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -83,8 +83,8 @@ INSERT INTO `ecrit` (`idUtil`, `idPost`) VALUES
 
 CREATE TABLE `groupe` (
   `id` int(11) NOT NULL,
-  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `nom` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -129,16 +129,16 @@ INSERT INTO `post` (`id`, `visibilite`, `importance`, `message`, `datepost`) VAL
 
 CREATE TABLE `utilisateur` (
   `id` int(11) NOT NULL,
-  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `prenom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `nom` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `prenom` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `motdepasse` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
-  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'image/profil.jpg',
-  `statut` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `filiere` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `annee` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `photo` varchar(150) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'image/profil.jpg',
+  `statut` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `filiere` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `annee` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
   `datenaissance` date NOT NULL,
-  `cv` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+  `cv` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --

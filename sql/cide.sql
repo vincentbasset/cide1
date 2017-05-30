80be728496e81d5005ef7d143617ebfe3084d052
Getsuren <vernay-jacques@hotmail.fr>
2017-05-30 14:48:30 +0200

    Add files via upload
    mise a jour des tables + quelques entrées type

--- sql/cide.sql
+++ sql/cide.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 29 Mai 2017 à 15:57
+-- Généré le :  Mar 30 Mai 2017 à 14:47
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.19
 
@@ -43,6 +43,15 @@ CREATE TABLE `appartient` (
   `droit` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Contenu de la table `appartient`
+--
+
+INSERT INTO `appartient` (`idUtil`, `idGroupe`, `droit`) VALUES
+(1, 1, 'membre'),
+(2, 1, 'membre'),
+(3, 1, 'admin');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +62,18 @@ CREATE TABLE `ecrit` (
   `idUtil` int(11) NOT NULL,
   `idPost` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `ecrit`
+--
+
+INSERT INTO `ecrit` (`idUtil`, `idPost`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(3, 5),
+(3, 6);
 
 -- --------------------------------------------------------
 
@@ -67,6 +88,13 @@ CREATE TABLE `groupe` (
   `description` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Contenu de la table `groupe`
+--
+
+INSERT INTO `groupe` (`id`, `nom`, `type`, `description`) VALUES
+(1, 'info-1A', 'default', 'filière informatique première année');
+
 -- --------------------------------------------------------
 
 --
@@ -75,10 +103,23 @@ CREATE TABLE `groupe` (
 
 CREATE TABLE `post` (
   `id` int(11) NOT NULL,
-  `visibilite` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `importance` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `message` text COLLATE utf8_unicode_ci NOT NULL
+  `visibilite` tinyint(1) NOT NULL,
+  `importance` tinyint(1) NOT NULL,
+  `message` text COLLATE utf8_unicode_ci NOT NULL,
+  `datepost` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `post`
+--
+
+INSERT INTO `post` (`id`, `visibilite`, `importance`, `message`, `datepost`) VALUES
+(1, 1, 1, 'message public et important', '2017-05-30 12:32:19'),
+(2, 0, 0, 'message non privée et non important', '2017-05-30 12:32:19'),
+(3, 0, 1, 'message privée mais important', '2017-05-30 12:33:04'),
+(4, 1, 0, 'message public mais non important', '2017-05-30 12:33:04'),
+(5, 1, 1, 'partielle 1A info incomming', '2017-05-30 12:45:46'),
+(6, 1, 0, 'message public non important de la part de votre délégué', '2017-05-30 12:45:46');
 
 -- --------------------------------------------------------
 
@@ -91,8 +132,22 @@ CREATE TABLE `utilisateur` (
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `prenom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `adresse` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `motdepasse` varchar(60) COLLATE utf8_unicode_ci NOT NULL
+  `motdepasse` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'image/profil.jpg',
+  `statut` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `filiere` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `annee` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `dateNaissance` date DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `adresse`, `motdepasse`, `photo`, `statut`, `filiere`, `annee`, `dateNaissance`) VALUES
+(1, 'Basset', 'Vincent', 'vincent.basset@uha.fr', 'jacquesetsunsalopard', 'image/profil.jpg', 'etudiant', 'info', '1A', '1995-07-01'),
+(2, 'Vernay', 'Jacques', 'jacques.vernay@uha.fr', 'vincentestmondieu', 'image/profil.jpg', 'etudiant', 'info', '1A', '1995-01-30'),
+(3, 'Planes', 'Louis', 'louis.planes@uha.fr', 'ichwill', 'image/profil.jpg', 'etudiant', 'info', '1A', '1996-09-04');
 
 --
 -- Index pour les tables exportées
@@ -144,17 +199,17 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `groupe`
 --
 ALTER TABLE `groupe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

40dcd0c57d1d62416b8795575aeaf7afb2ce4bda
fannyringler <fanny.ringler@uha.fr>
2017-05-30 11:32:07 +0200

    Add files via upload
    ajout de php sur header et affichage des postes de la base de donnees cide

--- cide/index.php
+++ cide/index.php
@@ -1,160 +1,39 @@
 <?php
 	include("header.php");
+	$date = $bdd -> query("select getdate() as currentdatetime");
+	$reponse = $bdd -> query("SELECT * FROM post inner join ecrit on post.id=ecrit.idPost inner join utilisateur on ecrit.idUtil=utilisateur.id order by datepost desc");
+	$reponse2 = $bdd -> query("SELECT * FROM post inner join ecrit on post.id=ecrit.idPost inner join utilisateur on ecrit.idUtil=utilisateur.id order by datepost desc");
 ?>
-			<div class="centre">
-			<p>
-				<span>
-				<img src="image/profil.jpg" title="Mr TrucMuche" alt="connard" width="50px" height="50px" />
-				<a href="profil.php">Mr TrucMuche</a>      <!--lien vers le profil de la personne-->
-				</span></br>
-				test test test test test test test test test test test test test
-				test test test test test test #1AINFO test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
 			
-			<p>
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
-			
-			<p>
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
-			<p>
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
-			<p>
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
-			<p>
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
-			<p>
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
-			<p>
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
-		</div>
+			<?php
+			echo "<div class=\"centre\">";
+				while($donnees=$reponse->fetch()){
+					echo "<p>
+						<span>
+						<img src=\"".$donnees["photo"]."\" title=\"".$donnees["nom"]." ".$donnees["prenom"]."\" alt=\"".$donnees["nom"]." ".$donnees["prenom"]."\" width=\"50px\" height=\"50px\" />
+						<a href=\"profil.php\">".$donnees["nom"]." ".$donnees["prenom"]."</a>      <!--lien vers le profil de la personne-->
+						</span></br>
+						".$donnees["message"]."
+						<span class=\"date\">Posté le ".$donnees["datepost"]."</span>
+						</p>
+						";
+				}
+				echo "</div><div class=\"droit\">";
+				while($donnees=$reponse2->fetch()){
+					if($donnees["importance"]){
+						echo "<p>
+						<span>
+						<img src=\"".$donnees["photo"]."\" title=\"".$donnees["nom"]." ".$donnees["prenom"]."\" alt=\"".$donnees["nom"]." ".$donnees["prenom"]."\" width=\"50px\" height=\"50px\" />
+						<a href=\"profil.php\">".$donnees["nom"]." ".$donnees["prenom"]."</a>      <!--lien vers le profil de la personne-->
+						</span></br>
+						".$donnees["message"]."
+						<span class=\"date\">Posté le ".$donnees["datepost"]."</span>
+						</p>";
+					}
+				}
+				echo "</div>";
+			?>
+
 		
-		<div class="droit">
-			<p>
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-				test test test test test test test test test test test test test
-			</p>
-		</div>
 	</body>
 </html>

4c4cb2727616d42268fc9dde6c4617c88356694c
fannyringler <fanny.ringler@uha.fr>
2017-05-30 17:39:09 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function check()
+{
+    var input = document.getElementByName("filiere");
+	alert(input);
+   /* // si un mot de passe a été entré
+    if ( password != null )
+    {
+    	// on le compare à celui attendu
+    	if ( password == "bravo" )
+    	{
+    	    // mot de passe valide, on ouvre la page secrète
+    	    document.location.href = "http://google.fr";
+        }
+        else
+        {
+            alert( "Mot de passe incorrecte!", "Erreur" );
+        }
+    }*/
+}

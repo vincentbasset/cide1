ff5f94b7cff95cdf568679a87d6a356ee56f8648
Getsuren <vernay-jacques@hotmail.fr>
2017-05-31 08:36:40 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function check()
-{
-    var password = document.forms["inscription"]["mot de passe"].value;
-	var confirm = document.forms["inscription"]["mdp verification"].value;
-	if(password != confirm){
-		alert("Votre mot de passe ne correspond pas à la confirmation");
-	}
-}

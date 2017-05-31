fc0d4e0e872e38c9a3523ca5996ca368f348b4cd
Getsuren <vernay-jacques@hotmail.fr>
2017-05-31 16:21:03 +0200

    update de l'index
    l'index change selon que l'on soit connecté ou non, si oui alors il include fluxdactu.php sinon il envoie un message poussant à l'inscription

--- cide/index.php
+++ cide/index.php
@@ -1,39 +1,11 @@
 <?php
-	include("header.php");
-	$date = $bdd -> query("select getdate() as currentdatetime");
-	$reponse = $bdd -> query("SELECT * FROM post inner join ecrit on post.id=ecrit.idPost inner join utilisateur on ecrit.idUtil=utilisateur.id order by datepost desc");
-	$reponse2 = $bdd -> query("SELECT * FROM post inner join ecrit on post.id=ecrit.idPost inner join utilisateur on ecrit.idUtil=utilisateur.id order by datepost desc");
-?>
-			
-			<?php
-			echo "<div class=\"centre\">";
-				while($donnees=$reponse->fetch()){
-					echo "<p>
-						<span>
-						<img src=\"".$donnees["photo"]."\" title=\"".$donnees["nom"]." ".$donnees["prenom"]."\" alt=\"".$donnees["nom"]." ".$donnees["prenom"]."\" width=\"50px\" height=\"50px\" />
-						<a href=\"profil.php\">".$donnees["nom"]." ".$donnees["prenom"]."</a>      <!--lien vers le profil de la personne-->
-						</span></br>
-						".$donnees["message"]."
-						<span class=\"date\">Posté le ".$donnees["datepost"]."</span>
-						</p>
-						";
-				}
-				echo "</div><div class=\"droit\">";
-				while($donnees=$reponse2->fetch()){
-					if($donnees["importance"]){
-						echo "<p>
-						<span>
-						<img src=\"".$donnees["photo"]."\" title=\"".$donnees["nom"]." ".$donnees["prenom"]."\" alt=\"".$donnees["nom"]." ".$donnees["prenom"]."\" width=\"50px\" height=\"50px\" />
-						<a href=\"profil.php\">".$donnees["nom"]." ".$donnees["prenom"]."</a>      <!--lien vers le profil de la personne-->
-						</span></br>
-						".$donnees["message"]."
-						<span class=\"date\">Posté le ".$donnees["datepost"]."</span>
-						</p>";
+    include("header.php");
+    if (isset($_SESSION['id'])) {
+                        //ce qui se passe si on est co
+                        include("fluxdactu.php");
 					}
-				}
-				echo "</div>";
-			?>
-
-		
-	</body>
-</html>
+					else {
+					   //ce qui ce passe si on est pas co, avec un exemple de formulaire liant à un fichier qui fera le login
+						echo '<a href="inscription.php">Pour avoir accès à ce contenue, inscrivez vous ;)</a>';
+					}
+?>

cd9f7717f805e3f65f8afb205b32afa5716b5926
fannyringler <fanny.ringler@uha.fr>
2017-06-01 09:15:18 +0200

    ajout de la barre de recherche
    n'oubliez pas de charger l'image de la loupe dans image

--- cide/navigation.php
+++ cide/navigation.php
@@ -2,4 +2,35 @@
 	<a href="index.php">Accueil</a>
 	<a href="groupe.php">Mes Groupes</a>
 	<a href="profil.php">Mon profil</a>
+	<form>
+		<input id="recherche" type="text" name="search" placeholder="Search..">
+	</form>
+	<?php
+		$url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";   
+		$recherche = substr($url, strpos($url, "?search=") + 8);
+		$mot1 = strtok($recherche, '+');
+		if (strpos($recherche, '+') !== false) {
+			$mot2 = substr($recherche, strpos($recherche, "+") + 1);
+		}
+		else{
+			$mot2="";
+		}
+		if (strpos($mot2, '+') !== false){
+			$mot2 = strtok($mot2, '+');
+			echo "trop de mot dans recherche<br/>";
+		}
+		$rechercheutil = $bdd -> query("SELECT * FROM utilisateur where nom like '%".$mot1."%' and prenom like '%".$mot2."%' or nom like '%".$mot2."%' and prenom like '%".$mot1."%'");
+		if(strpos($recherche, '+') !==false){
+			$recherchegroupe = $bdd -> query("SELECT * FROM groupe where nom like '%".$mot1."%".$mot2."%' or nom like '%".$mot2." ".$mot1."%'");
+		}
+		else{
+			$recherchegroupe = $bdd -> query("SELECT * FROM groupe where nom like '%".$recherche."%'");
+		}
+		while($donnees = $rechercheutil->fetch()){
+			echo $donnees["nom"]." ".$donnees["prenom"]."<br />";
+		}
+		while($donnees2 = $recherchegroupe->fetch()){
+			echo $donnees2["nom"]."<br />";
+		}
+	?>
 </nav>

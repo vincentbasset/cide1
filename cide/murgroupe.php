7304c5a9fdf51c3e676c5e995c3175a6dfe657ec
vincentbasset <vincent.basset@uha.fr>
2017-06-01 17:36:38 +0200

    distinction membre admin

--- cide/murgroupe.php
+++ cide/murgroupe.php
@@ -3,23 +3,31 @@
 	$reponse = $bdd -> query("SELECT * FROM post inner join ecrit on post.id=ecrit.idPost inner join utilisateur on ecrit.idUtil=utilisateur.id WHERE utilisateur.id=".$_SESSION['id']." order by datepost desc");
 	$reponse2 = $bdd -> query("SELECT * FROM post inner join ecrit on post.id=ecrit.idPost inner join utilisateur on ecrit.idUtil=utilisateur.id inner join appartient on utilisateur.id=appartient.idUtil inner join groupe on appartient.idGroupe=groupe.id WHERE groupe.nom in(SELECT groupe.nom FROM appartient inner join groupe on appartient.idGroupe=groupe.id WHERE appartient.idUtil=".$_SESSION['id'].") ORDER BY datepost DESC ");
 	$reponse3 = $bdd -> query("SELECT * FROM groupe WHERE groupe.id=1");
+	$reponse4 = $bdd -> query("SELECT * FROM appartient WHERE idGroupe=1 AND idUtil=".$_SESSION['id']." ");
+	
 ?>
 			<?php
 			echo "<div class=\"centre\">
-			<form method=\"post\">
+			<form method=\"post\" action=\"traitementmurg.php\">
 				<p>";
 					while($donnees=$reponse3->fetch()){
 					echo "
 					<img src=\"".$donnees["icone"]."\" title=\"".$donnees["nom"]."\" alt=\"".$donnees["nom"]."\" width=\"60px\" height=\"60px\" />
-					<span id=\"mgroupe\">".$donnees["nom"]."</span></br></br>";}			
+					<span id=\"mgroupe\">".$donnees["nom"]."</span></br>";}			
 					echo "
 					<label for=\"message\"></label> 
-					<textarea name=\"message\" cols=\"108\" rows=\"6\" placeholder=\"Poste un message pour le groupe\"></textarea>				
-					<input type=\"checkbox\" name=\"important\" required>
-					<label for=\"important\">important</label>
-					<input type=\"checkbox\" name=\"public\" required>
-					<label for=\"public\">public</label>
-					<input type=\"submit\" value =\"Envoyer\" name=\"envoi\"/>	
+					<textarea name=\"message\" cols=\"108\" rows=\"6\" placeholder=\"Poste un message pour le groupe\"></textarea>";				
+					while($donnees=$reponse4->fetch()){
+						if($donnees["droit"]=="admin"){
+							echo "
+							<input type=\"checkbox\" name=\"important\" required>
+							<label for=\"important\">important</label>
+							<input type=\"checkbox\" name=\"public\" required>
+							<label for=\"public\">public</label>";
+						}
+					}
+					echo"
+					<input type=\"submit\" value =\"Envoyer\" name=\"envoyer\"/>	
 				</p>
 			</form>
 		";
@@ -49,7 +57,5 @@
 				}
 				echo "</div>";
 			?>
-
-		
 	</body>
 </html>

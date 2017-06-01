7a85dc2b55a7e3058ca9a6108c3025a1205073a1
fannyringler <fanny.ringler@uha.fr>
2017-06-01 07:59:23 +0200

    changement style
    après concertation, changement de la classe de la div englobant le message venez vous inscrire de centre a centreprofil

--- cide/groupe.php
+++ cide/groupe.php
@@ -6,7 +6,7 @@
 					}
 					else {
 					   //ce qui ce passe si on est pas co, avec un exemple de formulaire liant à un fichier qui fera le login
-						echo '<div class="centre"><p><a class="inscription" href="inscription.php">Venez vous inscrire</a></p></div>';
+						echo '<div class="centreprofil"><p><a class="inscription" href="inscription.php">Venez vous inscrire</a></p></div>';
 					}
 	echo "</body>
 		</html>";

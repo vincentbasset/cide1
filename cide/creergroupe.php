2b763b5d65130d36943777ce548d4fd4362b99e1
Getsuren <vernay-jacques@hotmail.fr>
2017-06-02 10:42:08 +0200

    le créateur appartient au groupe
    le créateur du groupe devient membre du groupe avec les droit de createur

--- cide/creergroupe.php
+++ cide/creergroupe.php
@@ -7,6 +7,9 @@
 			$type = $_POST["type"];
 			$insertion = $bdd->prepare("INSERT INTO groupe VALUES(NULL,\"".$nom."\",\"".$type."\",\"".$description."\",\"image/icone.jpg\")");
 			$insertion->execute();
+			$id = $bdd->lastInsertId();
+           		$insertion = $bdd->prepare("INSERT INTO appartient VALUES(".$_SESSION['id'].",".$id.",\"createur\")");
+			$insertion->execute();
 		}
 	}
 ?>

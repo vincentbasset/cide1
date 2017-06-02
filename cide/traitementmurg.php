1b36db208d6f2a26c79456d0bbed73664dac14a1
Getsuren <vernay-jacques@hotmail.fr>
2017-06-02 09:28:28 +0200

    Update traitementmurg.php

--- cide/traitementmurg.php
+++ cide/traitementmurg.php
@@ -20,7 +20,7 @@
 			$insertion->execute();
 		}
 	}
-	echo '<meta http-equiv="refresh" content="0;URL=groupe.php?id='.$donnees["id"].'">';
+	echo '<meta http-equiv="refresh" content="0;URL=groupe.php?id='.$_GET["id"].'">';
 ?>
 	</body>
 </html>

5d695634039c34304e0d0aec7fa7cc56da1e8a5e
Getsuren <vernay-jacques@hotmail.fr>
2017-06-06 17:52:35 +0200

    Add files via upload

--- cide/navigation.php
+++ cide/navigation.php
@@ -1,22 +1,25 @@
-
 </br>
+<?php
+if (isset($_SESSION['id'])) {
+    $reponse=$bdd->query('SELECT DISTINCT idUtil2 FROM chat WHERE idUtil1='.$_SESSION['id'].'');
+}
+?>
 <nav>
 	<a href="index.php">Accueil</a>
 	<a href="groupe.php">Mes Groupes</a>
-	<?php
-	if (isset($_SESSION['id'])){
-	$reponse = $bdd -> query("SELECT * FROM utilisateur WHERE id=".$_SESSION['id']."");
-	$donnees=$reponse->fetch();
-		echo '<a href="murprofil.php?id='.htmlspecialchars($donnees["id"]).'">Mon mur</a>';
-	}
-	else{
-		echo '<a href="profil.php">Mon mur</a>';
-	}
-	?>
+	<a href="profil.php">Mon profil</a>
 	</br></br></br></br></br>
 	<form method="post" action="traitementrecherche.php">
 		<input id="recherche" type="text" name="search" placeholder="Rechercher..">
 	</form>
 	</br></br></br></br></br>
 	<a id="navcrea" href="creagroupe.php">Créer un groupe</a>
+    <?php 
+        if (isset($_SESSION['id'])) {
+            while($donnees=$reponse->fetch()){
+                include "chat.php";
+            } 
+        }
+    ?>
+</br></br></br>
 </nav>

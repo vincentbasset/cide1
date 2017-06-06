c9ca581a5091ffc3a16e9568f20038dd1a1da264
LouisPlanes <Louis.planes@gmail.com>
2017-06-06 16:02:15 +0200

    gestion des URL dans les liens généralisée

--- cide/traitementmurp.php
+++ cide/traitementmurp.php
@@ -6,8 +6,8 @@
 	if(isset($_POST["envoyer"])){
 		if(!empty($_POST["message"])){
 			$message = $_POST["message"];
-			$insertion = $bdd->prepare("INSERT INTO post VALUES(NULL,\"".$_SESSION['id']."\",0,".$_GET['id'].",0,0,0,\"".$message."\",\"\",CURRENT_TIMESTAMP)");
-			$insertion->execute();
+			$insertion = $bdd->prepare("INSERT INTO post VALUES(NULL,:iduser,0,:idpage,0,0,0,:message,:lien,CURRENT_TIMESTAMP)");
+			$insertion->execute(['iduser'=>$_SESSION['id'],'idpage'=>$_GET['id'],'message'=>$message, 'lien'=>$_POST['lien']]);
 		}
 	}
 	echo '<meta http-equiv="refresh" content="0;URL=murprofil.php?id='.htmlspecialchars($_GET["id"]).'">';

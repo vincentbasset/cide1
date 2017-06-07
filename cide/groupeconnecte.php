b9d2fc3dafb74c2a310e0ab81916c5f65d13c6b2
vincentbasset <vincent.basset@uha.fr>
2017-06-08 00:54:24 +0200

    un beau groupe connecte tout propre et mise a jour des liens nav

--- cide/groupeconnecte.php
+++ cide/groupeconnecte.php
@@ -1,49 +1,45 @@
 <?php
 		$reponse = $bdd -> query("SELECT groupe.* FROM utilisateur inner join appartient on appartient.idUtil=utilisateur.id inner join groupe on appartient.idGroupe=groupe.id  WHERE utilisateur.id=".$_SESSION['id']." ORDER BY groupe.nom");
 		$reponse2 = $bdd -> query("SELECT * FROM groupe where id not in (select groupe.id from utilisateur inner join appartient on appartient.idUtil=utilisateur.id inner join groupe on appartient.idGroupe=groupe.id  WHERE utilisateur.id=".$_SESSION['id']." ORDER BY groupe.nom)");
+		
 ?>
-	<div class="col-sm-1 col-perso">
+	<div class="centregroupe">
 		<h2>Mes groupes</h2>
-		</br>
-		</br>
 			<?php
 			while($donnees=$reponse->fetch()){
 				echo '
-					<a href="groupe.php?id='.htmlspecialchars($donnees["id"]).'">
-						<div class="media">
-							<div class="media-left">
-								<img class="img-circle" src="'.htmlspecialchars($donnees["icone"]).'" title="'.htmlspecialchars($donnees["nom"]).'" alt="'.htmlspecialchars($donnees["nom"]).'" width="80px" height="80px" />
-							</div>
-							<div class="media-body lgroupe">
-							<h4 class="media-heading lgroupe">'.htmlspecialchars($donnees["nom"]).'</h4>
-						
-						<p>'.htmlspecialchars($donnees["description"]).'</p>
-						</a>
-					<hr>';
-				
-			}
-			?>
-		<hr>
-		<h2>Rejoins un nouveau groupe!</h2>
-			</br>
-			</br>
-			</br>
-			<?php
-			while($donnees=$reponse2->fetch()){
-				echo '
-					<a href="groupe.php?id='.htmlspecialchars($donnees["id"]).'">
-						<div class="media">
-							<div class="media-left">
-								<img class="img-circle" src="'.htmlspecialchars($donnees["icone"]).'" title="'.htmlspecialchars($donnees["nom"]).'" alt="'.htmlspecialchars($donnees["nom"]).'" width="80px" height="80px" />
-							</div>
-							<div class="media-body lgroupe">
-							<h4 class="media-heading lgroupe">'.htmlspecialchars($donnees["nom"]).'</h4>
-						<p>'.htmlspecialchars($donnees["description"]).'</p>
-						<hr>
-					</a>';
+				<p id="gnomliste">
+				<a href="groupe.php?id='.htmlspecialchars($donnees["id"]).'">
+					<span>
+						<img src="'.htmlspecialchars($donnees["icone"]).'" title="'.htmlspecialchars($donnees["nom"]).'" alt="'.htmlspecialchars($donnees["nom"]).'" width="60px" height="60px" />
+							<span id="gnomliste2">'.htmlspecialchars($donnees["nom"]).'</span>
+					</span>
+					</br>
+					'.htmlspecialchars($donnees["description"]).'				
+				</a>
+				</p>
+				';
 			}
 			?>
 	</div>
-</div>
-</body>
+	<div class="centregroupeinf">
+		<h2>Rejoins un nouveau groupe!</h2>
+			<?php
+			while($donnees=$reponse2->fetch()){
+				echo '
+				<p>
+				<a href="groupe.php?id='.htmlspecialchars($donnees["id"]).'">
+					<span>
+						<img src="'.htmlspecialchars($donnees["icone"]).'" title="'.htmlspecialchars($donnees["nom"]).'" alt="'.htmlspecialchars($donnees["nom"]).'" width="60px" height="60px" />
+							'.htmlspecialchars($donnees["nom"]).'
+					</span>
+					</br>
+					'.htmlspecialchars($donnees["description"]).'				
+				</a>
+				</p>
+				';
+			}
+			?>
+	</div>
+	</body>
 </html>

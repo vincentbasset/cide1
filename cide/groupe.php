aae6fce9693fd890ea3fe8898e7acee737403e89
vincentbasset <vincent.basset@uha.fr>
2017-06-08 10:17:22 +0200

    changement div

--- cide/groupe.php
+++ cide/groupe.php
@@ -5,7 +5,7 @@
 		$donnees=$reponse->fetch();
 		if($donnees["accepte"] == "1"){
 			include("murgroupe.php");
-		}else{	echo '<div class="centredeco">
+		}else{	echo '<div class="col-sm-1 col-perso" id="deco">
 			<p>
 			En attente d\'acceptation
 			</p></div>';
@@ -17,7 +17,7 @@
 		}
 		else {
 			//ce qui ce passe si on est pas co, avec un exemple de formulaire liant à un fichier qui fera le login
-			echo '<div class="centredeco">
+			echo '<div <div class="col-sm-1 col-perso" id="deco">
 				<p>
 				Connecte toi ou rejoins nous !
 				</br>

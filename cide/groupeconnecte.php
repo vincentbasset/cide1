cf220dda0f6abcc42d1a98345bf4a797e358acc5
vincentbasset <vincent.basset@uha.fr>
2017-06-08 08:59:36 +0200

    nouveau site

--- cide/groupeconnecte.php
+++ cide/groupeconnecte.php
@@ -1,45 +1,49 @@
 <?php
 		$reponse = $bdd -> query("SELECT groupe.* FROM utilisateur inner join appartient on appartient.idUtil=utilisateur.id inner join groupe on appartient.idGroupe=groupe.id  WHERE utilisateur.id=".$_SESSION['id']." ORDER BY groupe.nom");
 		$reponse2 = $bdd -> query("SELECT * FROM groupe where id not in (select groupe.id from utilisateur inner join appartient on appartient.idUtil=utilisateur.id inner join groupe on appartient.idGroupe=groupe.id  WHERE utilisateur.id=".$_SESSION['id']." ORDER BY groupe.nom)");
-		
 ?>
-	<div class="centregroupe">
+	<div class="col-sm-1 col-perso">
 		<h2>Mes groupes</h2>
+		<br/>
+		<br/>
 			<?php
 			while($donnees=$reponse->fetch()){
 				echo '
-				<p id="gnomliste">
-				<a href="groupe.php?id='.htmlspecialchars($donnees["id"]).'">
-					<span>
-						<img src="'.htmlspecialchars($donnees["icone"]).'" title="'.htmlspecialchars($donnees["nom"]).'" alt="'.htmlspecialchars($donnees["nom"]).'" width="60px" height="60px" />
-							<span id="gnomliste2">'.htmlspecialchars($donnees["nom"]).'</span>
-					</span>
-					</br>
-					'.htmlspecialchars($donnees["description"]).'				
-				</a>
-				</p>
-				';
+					<hr>
+					<a href="groupe.php?id='.htmlspecialchars($donnees["id"]).'">
+						<div class="media" lgroupe>
+							<img class="img-circle" src="'.htmlspecialchars($donnees["icone"]).'" title="'.htmlspecialchars($donnees["nom"]).'" alt="'.htmlspecialchars($donnees["nom"]).'" width="80px" height="80px" />
+							<div class="media-body lgroupe">
+								<h4 class="media-heading lgroupe">'.htmlspecialchars($donnees["nom"]).'</h4>
+								<p>'.htmlspecialchars($donnees["description"]).'</p>
+							</div>
+						</div>
+					</a>';
 			}
 			?>
-	</div>
-	<div class="centregroupeinf">
+		<br/>
+		<br/>
+		<hr>
+		<hr>
+		<br/>
+		<br/>
 		<h2>Rejoins un nouveau groupe!</h2>
+			<br/>
+			<br/>
 			<?php
 			while($donnees=$reponse2->fetch()){
 				echo '
-				<p>
-				<a href="groupe.php?id='.htmlspecialchars($donnees["id"]).'">
-					<span>
-						<img src="'.htmlspecialchars($donnees["icone"]).'" title="'.htmlspecialchars($donnees["nom"]).'" alt="'.htmlspecialchars($donnees["nom"]).'" width="60px" height="60px" />
-							'.htmlspecialchars($donnees["nom"]).'
-					</span>
-					</br>
-					'.htmlspecialchars($donnees["description"]).'				
-				</a>
-				</p>
-				';
+					<hr>
+					<a href="groupe.php?id='.htmlspecialchars($donnees["id"]).'">
+						<div class="media">
+							<img class="img-circle" src="'.htmlspecialchars($donnees["icone"]).'" title="'.htmlspecialchars($donnees["nom"]).'" alt="'.htmlspecialchars($donnees["nom"]).'" width="80px" height="80px" />
+							<div class="media-body lgroupe">
+								<h4 class="media-heading lgroupe">'.htmlspecialchars($donnees["nom"]).'</h4>
+								<p>'.htmlspecialchars($donnees["description"]).'</p>
+							</div>
+						</div>
+					</a>';
 			}
 			?>
 	</div>
-	</body>
-</html>
+
--- cide/profilconnecte.php
+++ cide/profilconnecte.php
@@ -2,20 +2,20 @@
 	$reponse = $bdd -> query("SELECT * FROM utilisateur WHERE id=".$_SESSION['id']."");
 	$reponse2 = $bdd -> query("SELECT * FROM utilisateur inner join appartient on appartient.idUtil=utilisateur.id inner join groupe on appartient.idGroupe=groupe.id  WHERE utilisateur.id=".$_SESSION['id']." ORDER BY groupe.nom LIMIT 3");
 ?>
-	<div class="centreprofil">
+	<div class="col-sm-1 col-perso" id="styleprofil">
 		<h2>Mon profil</h2>
 		<p>
-			<div id="pgauche">
-					<?php
+			<div>
+				<?php
 					$pdonnees=$reponse->fetch();
 					echo'
 					<img src="'.htmlspecialchars($pdonnees["photo"]).'" title="'.htmlspecialchars($pdonnees["nom"]).' '.htmlspecialchars($pdonnees["prenom"]).'" alt="'.htmlspecialchars($pdonnees["nom"]).' '.htmlspecialchars($pdonnees["prenom"]).'" width="225px" height="225px" />
 					</br>			
 					</div>
-					<div id="pdroit">
+					<div>
 						<p>
 						<h3>'.htmlspecialchars($pdonnees["nom"]).'</h3>
-						</br>
+						
 						<h3>'.htmlspecialchars($pdonnees["prenom"]).'</h3>
 					</div>
 					</p>
@@ -36,40 +36,50 @@
 					
 					?>
 				<form method="post" action="traitementdescription.php">
-					<span id="bleu"><label for="description">Modifier ma description:</label></span>
-					<textarea name="description" rows="15" cols="30"></textarea>
+					<textarea name="description" rows="9" cols="75" placeholder="Modifier votre description"></textarea>
 					</br>
 					</br>
 					</br>
 					<input type="submit" name="envoyer" value="Changer la description"/>
 				</form>
+				</br>
+				</br>
 				<form method="post" action="traitementphoto.php" enctype="multipart/form-data">
 					<span id="bleu"><label for="photo">Modifier photo de profil:</label></span>
+					</br>
 					<input type="file" name="photo" accept="image/gif, image/jpeg, image/png">
+					</br>
 					</br>
 					<input type="submit" name="envoyer" value="Changer la photo"/>
 				</form>
 				</br>
+				</br>
 				<a href="changermdp.php">Changer de mot de passe</a>
 				</br>
+				</br>
 				<p>
-					<span id="bleu">groupes:</span>
+					<span>Groupes:</span>
 					</br>
 					
 					<?php
 					while($pdonnee=$reponse2->fetch()){
-					echo'<span id="glisteprofil">'.htmlspecialchars($pdonnee["nom"]).'</span></br>';
+					echo'<span>'.htmlspecialchars($pdonnee["nom"]).'</span></br>';
 					}
 					?>
-				</p>
+				...</p>
+				</br>
+				</br>
 				<p>
 					<?php
 					echo'<span id="bleu"><a href="'.htmlspecialchars($pdonnees["cv"]).'" target="_blank" >Mon CV</a></span></br>';
 					?>
 				</p>
+				</br>
 				<form method="post" action="traitementcv.php" enctype="multipart/form-data">
-					<span id="bleu"><label for="cv">Ajouter une photo de profil:</label></span>
+					<span id="bleu"><label for="cv">Ajouter une photo de CV:</label></span>
+					</br>
 					<input type="file" name="cv" accept="application/pdf">
+					</br>
 					</br>
 					<input type="submit" name="envoyer" value="Changer le CV"/>
 				</form>

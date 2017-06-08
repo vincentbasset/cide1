c16eaee24d08ce2e16120eade25abbcbd8ac5cc1
vincentbasset <vincent.basset@uha.fr>
2017-06-08 15:46:57 +0200

    recherche tout beau tout propre

--- cide/recherche.php
+++ cide/recherche.php
@@ -3,7 +3,7 @@
 		$url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";   
 		$recherche = substr($url, strpos($url, "?search=") + 8);
 		$mot1 = strtok($recherche, '%20');
-		echo "<div class=\"centreprofil\"> <p>";
+		echo "<div class=\"col-sm-1 col-perso\" id=\"rech\"><p>";
 		if (strpos($recherche, '%20') !== false) {
 			$mot2 = substr($recherche, strpos($recherche, "%20") + 3);
 		}
@@ -22,10 +22,10 @@
 			$recherchegroupe = $bdd -> query("SELECT * FROM groupe where nom like '%".$recherche."%'");
 		}
 		while($donnees = $rechercheutil->fetch()){
-		echo "<a href=\"murprofil.php?id=".htmlspecialchars($donnees["id"])."\"><img src=\"".htmlspecialchars($donnees["photo"])."\" alt=\"".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."\" width=\"50px\" height=\"50px\"/>".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."</a><br />";
+		echo "<a href=\"murprofil.php?id=".htmlspecialchars($donnees["id"])."\"><img class=\"img-circle\"  src=\"".htmlspecialchars($donnees["photo"])."\" alt=\"".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."\" width=\"70px\" height=\"70px\"/><br/>".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."</a><br/><hr><br/>";
 		}
 		while($donnees2 = $recherchegroupe->fetch()){
-			echo "<a href=\"groupe.php?id=".htmlspecialchars($donnees2["id"])."\"><img src=\"".htmlspecialchars($donnees2["icone"])."\" alt=\"".htmlspecialchars($donnees["nom"])."\" width=\"50px\" height=\"50px\"/>".htmlspecialchars($donnees2["nom"])."</a><br />";
+			echo "<a href=\"groupe.php?id=".htmlspecialchars($donnees2["id"])."\"><img class=\"img-circle\" src=\"".htmlspecialchars($donnees2["icone"])."\" alt=\"".htmlspecialchars($donnees["nom"])."\" width=\"70px\" height=\"70px\"/><br/>".htmlspecialchars($donnees2["nom"])."</a><br/><hr><br/>";
 		}
 		echo "</p></div>";
 ?>

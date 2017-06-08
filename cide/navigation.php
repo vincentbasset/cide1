1122aa961beaae97a7dce7ed413cee39749a9991
vincentbasset <vincent.basset@uha.fr>
2017-06-08 08:48:13 +0200

    nouvelle nav

--- cide/navigation.php
+++ cide/navigation.php
@@ -1,25 +1,43 @@
-</br>
-<?php
-if (isset($_SESSION['id'])) {
-    $reponse=$bdd->query('SELECT DISTINCT idUtil2 FROM chat WHERE idUtil1='.$_SESSION['id'].'');
-}
-?>
-<nav>
-	<a href="index.php">Accueil</a>
-	<a href="groupe.php">Mes Groupes</a>
-	<a href="profil.php">Mon profil</a>
-	</br></br></br></br></br>
-	<form method="post" action="traitementrecherche.php">
-		<input id="recherche" type="text" name="search" placeholder="Rechercher..">
-	</form>
-	</br></br></br></br></br>
-	<a id="navcrea" href="creagroupe.php">Créer un groupe</a>
-    <?php 
-        if (isset($_SESSION['id'])) {
-            while($donnees=$reponse->fetch()){
-                include "chat.php";
-            } 
-        }
-    ?>
-</br></br></br>
+<div class="container-fluid" style="background-color:rgb(43,87,160);color:#fff;height:200px;">
+  <img src="image/logo.jpg" title="Le Cercle d'Ingénieurs de l'ENSISA" alt="C.I.D.E." width="300px" height="200px" />
+</div>
+
+<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="200">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="index.php">CIDE</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+		<li><a href="https://cas.uha.fr/cas/login?service=http://www.e-services.uha.fr" target="_blank">ENSISA</a></li>
+		<li><a href="index.php">Accueil</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Groupe <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="groupe.php">Mes Groupes</a></li>
+            <li><a href="creagroupe.php">Créer Groupe</a></li>
+          </ul>
+        </li>
+        <li><?php
+ 			if (isset($_SESSION['id'])){
+					$reponse = $bdd -> query("SELECT * FROM utilisateur WHERE id=".$_SESSION['id']."");
+					$donnees=$reponse->fetch();
+					echo '<a href="murprofil.php?id='.htmlspecialchars($donnees["id"]).'">Mon mur</a>';
+				}
+				else{
+					echo '<a href="profil.php">Mon mur</a>';
+				}
+			?>
+		</li>
+      </ul>
+    </div>
+  </div>
 </nav>
+
+
+

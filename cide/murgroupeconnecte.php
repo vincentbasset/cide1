fa32964283157c31487bf182c8dde78ce4b48ef7
LouisPlanes <Louis.planes@gmail.com>
2017-06-09 09:44:31 +0200

    ajout limitation groupe

--- cide/murgroupeconnecte.php
+++ cide/murgroupeconnecte.php
@@ -18,20 +18,20 @@
 	if (!$reponse4->rowcount()==0){
 		echo "
 		<form method=\"post\" action=\"traitementmurg.php?id=".htmlspecialchars($_GET['id'])."\">";
-			$donnees=$reponse3->fetch();
+			$donneesgroupe=$reponse3->fetch();
 			echo "
 				<div class=\"media\">
 					<div class=\"media-left\">
-						<img class=\"img-circle\" src=\"".$donnees["icone"]."\" title=\"".htmlspecialchars($donnees["nom"])."\" alt=\"".htmlspecialchars($donnees["nom"])."\" width=\"90px\" height=\"90px\" />
+						<img class=\"img-circle\" src=\"".$donneesgroupe["icone"]."\" title=\"".htmlspecialchars($donneesgroupe["nom"])."\" alt=\"".htmlspecialchars($donneesgroupe["nom"])."\" width=\"90px\" height=\"90px\" />
 					</div>
 					<div class=\"media-body\">
-						<h3 class=\"media-heading\">".htmlspecialchars($donnees["nom"])."</h3>						
+						<h3 class=\"media-heading\">".htmlspecialchars($donneesgroupe["nom"])."</h3>						
 						<label for=\"lien\"></label> 
 						<input type=\"varchar\" name=\"lien\" placeholder=\"insére un lien ici\">			
 						<label for=\"message\"></label> 
 						<textarea id=\"textgroupe\" name=\"message\" cols=\"85\" rows=\"4\" placeholder=\"Poste un message pour le groupe\"></textarea><br/>";				
-						while($donnees=$reponse4->fetch()){
-							if($donnees["droit"]!="membre"){
+						while($donneesdroit=$reponse4->fetch()){
+							if($donneesdroit["droit"]!="membre"){
 								echo "
 									<input type=\"checkbox\" name=\"important\" />
 									<label for=\"important\">important</label>
@@ -43,97 +43,99 @@
 						<input type=\"submit\" value =\"Envoyer\" name=\"envoyer\"/>
 					</div>
 			</div>
-				</form>";
+            </form>";
 	}
 	echo "<hr><hr>";
-		$don=$reponse5->fetchAll();
-		while($donnees=$reponse->fetch()){
-			if (!$reponse4->rowcount()==0 || $donnees["visibilite"]==1){
-				echo "
-				<div class=\"media\">
-					<div class=\"media-left\">
-						<img class=\"img-circle\" src=\"".htmlspecialchars($donnees["photo"])."\" title=\"".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."\" alt=\"".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."\" width=\"60px\" height=\"60px\" />
-					</div>
-					<div class=\"media-body\">
-						<h4 class=\"media-heading\"><a href=\"murprofil.php?id=".htmlspecialchars($donnees["id"])."\">".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."</a></h4>";
-						if (preg_match("/www\.youtube\.com/",$donnees["url"])===1||preg_match("/youtu\.be/",$donnees["url"])===1){
-							$vid=preg_replace("/.*[=\/]/","",$donnees["url"]);
-							echo '<iframe width="420" height="315"
-								src="https://www.youtube.com/embed/'.$vid.'">
-								</iframe></br>';
-						} else if (preg_match("/.jpg$/",$donnees["url"]) === 1 || preg_match("/.png$/",$donnees["url"]) === 1 || preg_match("/.gif$/",$donnees["url"]) === 1 || preg_match("/.jpeg$/",$donnees["url"]) === 1){
-							echo "<img src=\"".htmlspecialchars($donnees["url"])."\" width=\"100%\" /></br>";
-						}else{
-							echo "<a href=".htmlspecialchars($donnees["url"]).">".htmlspecialchars($donnees["url"])."</a>";
-						}
-						echo "<p>".nl2br(htmlspecialchars($donnees["message"]))."</p>
-								<div class=\"date\">Posté le ".date_format(date_create_from_format("Y-m-j H:i:s",htmlspecialchars($donnees["datepost"])), "j/m/y \à G\hi")."</div>";
+    $don=$reponse5->fetchAll();
+    while($donneespost=$reponse->fetch()){
+        if (!$reponse4->rowcount()==0 || $donneespost["visibilite"]==1){
+            echo "
+            <div class=\"media\">
+                <div class=\"media-left\">
+                    <img class=\"img-circle\" src=\"".htmlspecialchars($donneespost["photo"])."\" title=\"".htmlspecialchars($donneespost["nom"])." ".htmlspecialchars($donneespost["prenom"])."\" alt=\"".htmlspecialchars($donneespost["nom"])." ".htmlspecialchars($donneespost["prenom"])."\" width=\"60px\" height=\"60px\" />
+                </div>
+                <div class=\"media-body\">
+                    <h4 class=\"media-heading\"><a href=\"murprofil.php?id=".htmlspecialchars($donnees["id"])."\">".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donneespost["prenom"])."</a></h4>";
+                    if (preg_match("/www\.youtube\.com/",$donneespost["url"])===1||preg_match("/youtu\.be/",$donneespost["url"])===1){
+                        $vid=preg_replace("/.*[=\/]/","",$donneespost["url"]);
+                        echo '<iframe width="420" height="315"
+                            src="https://www.youtube.com/embed/'.$vid.'">
+                            </iframe></br>';
+                    } else if (preg_match("/.jpg$/",$donneespost["url"]) === 1 || preg_match("/.png$/",$donneespost["url"]) === 1 || preg_match("/.gif$/",$donneespost["url"]) === 1 || preg_match("/.jpeg$/",$donneespost["url"]) === 1){
+                        echo "<img src=\"".htmlspecialchars($donneespost["url"])."\" width=\"100%\" /></br>";
+                    }else{
+                        echo "<a href=".htmlspecialchars($donneespost["url"]).">".htmlspecialchars($donneespost["url"])."</a>";
+                    }
+                    echo "<p>".nl2br(htmlspecialchars($donneespost["message"]))."</p>
+                            <div class=\"date\">Posté le ".date_format(date_create_from_format("Y-m-j H:i:s",htmlspecialchars($donneespost["datepost"])), "j/m/y \à G\hi")."</div>";
 
-						
-				
-				foreach($don as $donnee){
-					if($donnees["postid"]==$donnee["idPost"]){
-						echo"<hr>
-						<div class=\"media\">
-							<div class=\"media-left\">
-								<img class=\"img-circle\" src=\"".htmlspecialchars($donnee["photo"])."\" title=\"".htmlspecialchars($donnee["nom"])." ".htmlspecialchars($donnee["prenom"])."\" alt=\"".htmlspecialchars($donnee["nom"])." ".htmlspecialchars($donnee["prenom"])."\" width=\"50px\" height=\"50px\" />
-							</div>
-							<div class=\"media-body\">
-								<h4 class=\"media-heading\"><a href=\"murprofil.php?id=".htmlspecialchars($donnee["id"])."\">".htmlspecialchars($donnee["nom"])." ".htmlspecialchars($donnee["prenom"])."</a></h4>
-								<p>".nl2br(htmlspecialchars($donnee["message"]))."</p>
-								<div class=\"date\">Posté le ".date_format(date_create_from_format("Y-m-j H:i:s",htmlspecialchars($donnee["datepost"])), "j/m/y \à G\hi")."</div>
-							</div>
-						</div>";
-					}
-				
-				}
-					
-				echo"<form method=\"post\" action=\"traitementrep.php?id=".htmlspecialchars($donnees["postid"])."\">
-						<label for=\"message\"></label> 
-						<textarea name=\"message\" cols=\"90\" rows=\"4\" placeholder=\"Laisse un message !\"></textarea>						
-						<input type=\"submit\" value =\"Envoyer\" name=\"envoyer\"/>
-					</form>		
-					</div>
-					<hr>
-					</div>";		
-                    
-			}	
-		}
-		
-		echo"</div>";				
-					
-		echo "<div class=\"col-sm-3 col-perso\">";
-			if($reponse4->rowcount()==0){
-				echo"	
-					<form method=\"post\" action=\"traitementrejoindreg.php?id=".htmlspecialchars($_GET['id'])."\">
-						<input id=\"rejoindreg\" type=\"submit\"  value=\"Rejoindre\" name=\"rejoindre\">
-					</form>";
-			}
-			else{
-				echo"
-				<form method=\"post\" action=\"traitementquitterg.php?id=".htmlspecialchars($_GET['id'])."\">
-					<input id=\"rejoindreg\" type=\"submit\"  value=\"  Quitter  \" name=\"quitter\">
-				</form>";
-				while($donnees=$reponse6->fetch()){
-					if($donnees["droit"]!="membre"){
-                        echo "<a id=\"liengestion\"href='Gestiong.php?id=".$_GET['id']."'>Gérer le groupe</a><br><br>";
-					}
-               	}
-			}
-			while($donnees=$reponse2->fetch()){
-				if((!$reponse4->rowcount()==0 || $donnees["visibilite"]==1) && $donnees["importance"]){
-					echo "
-						<div class=\"media\">
-							<div class=\"media-left\">
-								<img class=\"img-circle\" src=\"".htmlspecialchars($donnees["photo"])."\" title=\"".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."\" alt=\"".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."\" width=\"60px\" height=\"60px\" />
-							</div>
-							<div class=\"media-body\">							
-								<h4 class=\"media-heading\"><a href=\"murprofil.php?id=".htmlspecialchars($donnees["id"])."\">".htmlspecialchars($donnees["nom"])." ".htmlspecialchars($donnees["prenom"])."</a></h4>
-								<p>".htmlspecialchars($donnees["message"])."</p>
-								<div class=\"date\">Posté le ".date_format(date_create_from_format("Y-m-j H:i:s",htmlspecialchars($donnee["datepost"])), "j/m/y \à G\hi")."</div>		
-							</div>
-						</div>";
-				}
-			}
+
+
+            foreach($don as $donneesreponses){
+                if($donneespost["postid"]==$donneesreponses["idPost"]){
+                    echo"<hr>
+                    <div class=\"media\">
+                        <div class=\"media-left\">
+                            <img class=\"img-circle\" src=\"".htmlspecialchars($donneesreponses["photo"])."\" title=\"".htmlspecialchars($donneesreponses["nom"])." ".htmlspecialchars($donneesreponses["prenom"])."\" alt=\"".htmlspecialchars($donneesreponses["nom"])." ".htmlspecialchars($donneesreponses["prenom"])."\" width=\"50px\" height=\"50px\" />
+                        </div>
+                        <div class=\"media-body\">
+                            <h4 class=\"media-heading\"><a href=\"murprofil.php?id=".htmlspecialchars($donneesreponses["id"])."\">".htmlspecialchars($donneesreponses["nom"])." ".htmlspecialchars($donneesreponses["prenom"])."</a></h4>
+                            <p>".nl2br(htmlspecialchars($donneesreponses["message"]))."</p>
+                            <div class=\"date\">Posté le ".date_format(date_create_from_format("Y-m-j H:i:s",htmlspecialchars($donneesreponses["datepost"])), "j/m/y \à G\hi")."</div>
+                        </div>
+                    </div>";
+                }
+
+            }
+
+            echo"<form method=\"post\" action=\"traitementrep.php?id=".htmlspecialchars($donneespost["postid"])."\">
+                    <label for=\"message\"></label> 
+                    <textarea name=\"message\" cols=\"90\" rows=\"4\" placeholder=\"Laisse un message !\"></textarea>						
+                    <input type=\"submit\" value =\"Envoyer\" name=\"envoyer\"/>
+                </form>		
+                </div>
+                <hr>
+                </div>";		
+
+        }	
+    }
+
+    echo"</div>";				
+
+    echo "<div class=\"col-sm-3 col-perso\">";
+    if($reponse4->rowcount()==0){
+        if($donneesgroupe["type"]!="defaut"){
+            echo"	
+                <form method=\"post\" action=\"traitementrejoindreg.php?id=".htmlspecialchars($_GET['id'])."\">
+                    <input id=\"rejoindreg\" type=\"submit\"  value=\"Rejoindre\" name=\"rejoindre\">
+                </form>";
+        }
+    }
+    else{
+        echo"
+        <form method=\"post\" action=\"traitementquitterg.php?id=".htmlspecialchars($_GET['id'])."\">
+            <input id=\"rejoindreg\" type=\"submit\"  value=\"  Quitter  \" name=\"quitter\">
+        </form>";
+        while($donneesmembre=$reponse6->fetch()){
+            if($donneesmembre["droit"]!="membre"){
+                echo "<a id=\"liengestion\"href='Gestiong.php?id=".$_GET['id']."'>Gérer le groupe</a><br><br>";
+            }
+        }
+    }
+    while($donneespostutil=$reponse2->fetch()){
+        if((!$reponse4->rowcount()==0 || $donneespostutil["visibilite"]==1) && $donneespostutil["importance"]){
+            echo "
+                <div class=\"media\">
+                    <div class=\"media-left\">
+                        <img class=\"img-circle\" src=\"".htmlspecialchars($donneespostutil["photo"])."\" title=\"".htmlspecialchars($donneespostutil["nom"])." ".htmlspecialchars($donneespostutil["prenom"])."\" alt=\"".htmlspecialchars($donneespostutil["nom"])." ".htmlspecialchars($donneespostutil["prenom"])."\" width=\"60px\" height=\"60px\" />
+                    </div>
+                    <div class=\"media-body\">							
+                        <h4 class=\"media-heading\"><a href=\"murprofil.php?id=".htmlspecialchars($donneespostutil["id"])."\">".htmlspecialchars($donneespostutil["nom"])." ".htmlspecialchars($donneespostutil["prenom"])."</a></h4>
+                        <p>".htmlspecialchars($donneespostutil["message"])."</p>
+                        <div class=\"date\">Posté le ".date_format(date_create_from_format("Y-m-j H:i:s",htmlspecialchars($donneespostutil["datepost"])), "j/m/y \à G\hi")."</div>		
+                    </div>
+                </div>";
+        }
+    }
 	echo "</div>";
 ?>

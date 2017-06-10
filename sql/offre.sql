bdd32a32f49911ff0e91bc5456a5804367e683e0
vincentbasset <vincent.basset@uha.fr>
2017-06-10 20:08:38 +0200

    La table offre!

---
+++ sql/offre.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Sam 10 Juin 2017 à 20:04
+-- Version du serveur :  10.1.21-MariaDB
+-- Version de PHP :  5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `cide`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offre`
+--
+
+CREATE TABLE `offre` (
+  `id` int(11) NOT NULL,
+  `idUtil` int(11) NOT NULL,
+  `nom` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `metier` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `lieu` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `nature` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `duree` int(11) NOT NULL,
+  `mobilite` tinyint(1) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `filiere` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `offre`
+--
+
+INSERT INTO `offre` (`id`, `idUtil`, `nom`, `metier`, `lieu`, `nature`, `duree`, `mobilite`, `description`, `filiere`) VALUES
+(1, 116, 'nom', 'metier', 'lieu', 'stage', 0, 1, 'description', 'stage');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `offre`
+--
+ALTER TABLE `offre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `offre`
+--
+ALTER TABLE `offre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

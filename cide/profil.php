0f87dea241c9f7002e61f431acd9d95bbd349c35
vincentbasset <vincent.basset@uha.fr>
2017-06-10 13:06:58 +0200

    Create profil.php

--- cide/profil.php
+++ cide/profil.php
@@ -5,7 +5,7 @@
 		include("profilconnecte.php");
 	}
 	else {
-		//ce qui ce passe si on est pas co, avec un exemple de formulaire liant à un fichier qui fera le login
+		//ce qui ce passe si on est pas co, avec un exemple de formulaire liant Ã  un fichier qui fera le login
 		echo '<div class="col-sm-1 col-perso" id="deco">
 				<p>
 				Connecte toi ou rejoins nous !
@@ -14,6 +14,8 @@
 				<a class="inscription" href="inscription.php">Pas encore inscrit ? Par ici !</a>
 				</p></div>';
 	}
-	echo "</body>
+	echo "
+	</div>
+	</body>
 		  </html>";
 ?>

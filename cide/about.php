f7e5bd16058a70743264019a4fec5fbdf9ddeae2
vincentbasset <vincent.basset@uha.fr>
2017-06-11 20:02:18 +0200

    w3c

--- cide/about.php
+++ cide/about.php
@@ -2,20 +2,27 @@
 	include("header.php");
 ?>
 		<div class="col-sm-1 col-perso" id="about">
-			<h2>A propos de C.I.D.E.</h2><br/>
-			<h3>Information sur l'entreprise</h3><br/>
+			<h2>A propos de C.I.D.E.</h2>
+			<br>
+			<h3>Information sur le site</h3>
+			<br>
 			<p>
-			Fondée en 2017, la mission de C.I.D.E. est de rassembler toutes personnes liées à l'ENSISA pour leur permettre d'échanger entre eux afin de leur assurer une meilleure
-			qualité de vie étudiante. C.I.D.E. propose alors ce réseau social pour qu'ils puissent apprécier au maximum leurs années au sein de l'ENSISA.
-			<br/><br/>
-			C.I.D.E. est composé de quatre élèves de l'ENSISA: Vincent Basset, Jacques Vernay, Louis Planes et Fanny Ringler
+			Fondée en 2017, la mission de C.I.D.E. est de rassembler toutes personnes liées à l'ENSISA pour leur permettre d'échanger entre eux, afin de leur assurer une meilleure
+			qualité de vie étudiante ou professionnel. C.I.D.E. propose alors ce réseau social pour qu'ils puissent apprécier au maximum leurs années au sein de l'ENSISA.
+			<br>
+			<br>
+			Quatre élèves de l'ENSISA sont à l'origine de C.I.D.E.: Vincent Basset, Jacques Vernay, Louis Planes et Fanny Ringler.
 			</p>
-			<h3>En savoir plus</h3><br/>
+			<h3>En savoir plus</h3>
+			<br>
 			<p>
-				Vous pouvez nous contacter par mail à l'adresse : <a href="mailto:contact@cide.fr">contact@cide.fr</a>
-				<br/>Ou par téléphone au: 0312456789
-				<br/>Où nous aurons le plaisir de vous répondre.
+				Vous pouvez nous contacter par mail à l'adresse: <a href="mailto:contact@cide.fr">contact@cide.fr</a>
+				<br>
+				Ou par téléphone au: 03 12 45 67 89
+				<br>
+				Où nous aurons le plaisir de vous répondre.
 			</p>
 		</div>
+	</div>	
 	</body>
 </html>

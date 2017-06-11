e29a4895861dd50984a0f0991058b3dd85d93045
vincentbasset <vincent.basset@uha.fr>
2017-06-11 20:06:25 +0200

    w3c

--- cide/cgu.php
+++ cide/cgu.php
@@ -3,66 +3,66 @@
 ?>
 		<div class="col-sm-1 col-perso">
 			<p>
-				<h2>Conditions générales d'utilisation du site CIDE</h2><br/><br/><br/>
+				<h2>Conditions générales d'utilisation du site CIDE</h2><br><br><br>
 				<h3>ARTICLE 1 : Objet</h3><p>
 				Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique des modalités de mise à disposition des services du site CIDE et leur utilisation par « l'Utilisateur ».
 				Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur souhaitant accéder au site. Elles constituent le contrat entre le site et l'Utilisateur. L’accès au site par l’Utilisateur signifie son acceptation des présentes conditions générales d’utilisation.
-				<br/>•	En cas de non-acceptation des conditions générales d'utilisation stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.
-				<br/>•	CIDE se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes conditions générales d'utilisation.
-				<br/><br/></p><h3>ARTICLE 2 : Mentions légales</h3><p>
+				<br>•	En cas de non-acceptation des conditions générales d'utilisation stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès des services proposés par le site.
+				<br>•	CIDE se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes conditions générales d'utilisation.
+				<br><br></p><h3>ARTICLE 2 : Mentions légales</h3><p>
 				L'édition du site CIDE est assurée par la Société CIDE SARL au capital de 42 000 € dont le siège social est situé au 12 Rue des Frères Lumière, 68093 Mulhouse, France.
 				Le Directeur de la publication est Monsieur Basset Vincent.
-				<br/><br/></p><h3>ARTICLE 3 : Définitions</h3><p>
+				<br><br></p><h3>ARTICLE 3 : Définitions</h3><p>
 				La présente clause a pour objet de définir les différents termes essentiels du contrat :
-				<br/>•	Utilisateur : ce terme désigne toute personne qui utilise le site ou l'un des services proposés par le site.
-				<br/>•	Contenu utilisateur : ce sont les données transmises par l'Utilisateur au sein du site.
-				<br/>•	Membre : l'Utilisateur devient membre lorsqu'il est identifié sur le site.
-				<br/>•	Identifiant et mot de passe : c'est l'ensemble des informations nécessaires à l'identification d'un Utilisateur sur le site. L'identifiant et le mot de passe permettent à l'Utilisateur d'accéder à des services réservés aux membres du site. Le mot de passe est confidentiel.
-				<br/><br/></p><h3>ARTICLE 4 : Accès aux services</h3><p>
+				<br>•	Utilisateur : ce terme désigne toute personne qui utilise le site ou l'un des services proposés par le site.
+				<br>•	Contenu utilisateur : ce sont les données transmises par l'Utilisateur au sein du site.
+				<br>•	Membre : l'Utilisateur devient membre lorsqu'il est identifié sur le site.
+				<br>•	Identifiant et mot de passe : c'est l'ensemble des informations nécessaires à l'identification d'un Utilisateur sur le site. L'identifiant et le mot de passe permettent à l'Utilisateur d'accéder à des services réservés aux membres du site. Le mot de passe est confidentiel.
+				<br><br></p><h3>ARTICLE 4 : Accès aux services</h3><p>
 				Le site permet à l'Utilisateur un accès gratuit aux services suivants :
-				<br/>•	Annonces classées ;
-				<br/>•	Mise en relation de personnes ;
-				<br/>•	Publication de commentaires / d’œuvres personnelles ;
-				<br/>•	Partage de documents.
+				<br>•	Annonces classées ;
+				<br>•	Mise en relation de personnes ;
+				<br>•	Publication de commentaires / d’œuvres personnelles ;
+				<br>•	Partage de documents.
 				Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.
 				L’Utilisateur non membre n'a pas accès aux services réservés aux membres. Pour cela, il doit s'identifier à l'aide de son identifiant et de son mot de passe.
-				<br/>
+				<br>
 				Le site met en œuvre tous les moyens mis à sa disposition pour assurer un accès de qualité à ses services. L'obligation étant de moyens, le site ne s'engage pas à atteindre ce résultat.
 				Tout événement dû à un cas de force majeure ayant pour conséquence un dysfonctionnement du réseau ou du serveur n'engage pas la responsabilité de CIDE.
 				L'accès aux services du site peut à tout moment faire l'objet d'une interruption, d'une suspension, d'une modification sans préavis pour une maintenance ou pour tout autre cas. L'Utilisateur s'oblige à ne réclamer aucune indemnisation suite à l'interruption, à la suspension ou à la modification du présent contrat.
 				L'Utilisateur a la possibilité de contacter le site par messagerie électronique à l’adresse contact@cide.com. 
-				<br/><br/></p><h3>ARTICLE 5 : Propriété intellectuelle</h3><p>
+				<br><br></p><h3>ARTICLE 5 : Propriété intellectuelle</h3><p>
 				Les marques, logos, signes et tout autre contenu du site font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.
 				L'Utilisateur sollicite l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus.
 				L'Utilisateur s'engage à une utilisation des contenus du site dans un cadre strictement privé. Une utilisation des contenus à des fins commerciales est strictement interdite.
 				Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur. 
 				Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site. L'Utilisateur ne reçoit aucune justification et notification préalablement à la suppression ou à la modification du contenu Utilisateur.
-				<br/><br/></p><h3>ARTICLE 6 : Données personnelles</h3><p>
+				<br><br></p><h3>ARTICLE 6 : Données personnelles</h3><p>
 				Les informations demandées à l’inscription au site sont nécessaires et obligatoires pour la création du compte de l'Utilisateur. En particulier, l'adresse électronique pourra être utilisée par le site pour l'administration, la gestion et l'animation du service.
 				Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. Le site est déclaré à la CNIL sous le numéro 012 - 345.
 				En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :
-				<br/>•	son espace personnel ;
-				<br/>•	un formulaire de contact ;
-				<br/>•	par mail à info@gouv.fr;
-				<br/>•	par voie postale au 68 bis rue de la victoire 75001 Paris.
-				<br/><br/></p><h3>ARTICLE 7 : Responsabilité et force majeure</h3><p>
+				<br>•	son espace personnel ;
+				<br>•	un formulaire de contact ;
+				<br>•	par mail à info@gouv.fr;
+				<br>•	par voie postale au 68 bis rue de la victoire 75001 Paris.
+				<br><br></p><h3>ARTICLE 7 : Responsabilité et force majeure</h3><p>
 				Les sources des informations diffusées sur le site sont réputées fiables. Toutefois, le site se réserve la faculté d'une non-garantie de la fiabilité des sources. Les informations données sur le site le sont à titre purement informatif. Ainsi, l'Utilisateur assume seul l'entière responsabilité de l'utilisation des informations et contenus du présent site.
 				L'Utilisateur s'assure de garder son mot de passe secret. Toute divulgation du mot de passe, quelle que soit sa forme, est interdite.
 				L'Utilisateur assume les risques liés à l'utilisation de son identifiant et mot de passe. Le site décline toute responsabilité.
 				Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.
 				Une garantie optimale de la sécurité et de la confidentialité des données transmises n'est pas assurée par le site. Toutefois, le site s'engage à mettre en œuvre tous les moyens nécessaires afin de garantir au mieux la sécurité et la confidentialité des données.
 				La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.
-				<br/><br/></p><h3>ARTICLE 8 : Liens hypertextes</h3><p>
+				<br><br></p><h3>ARTICLE 8 : Liens hypertextes</h3><p>
 				De nombreux liens hypertextes sortants sont présents sur le site, cependant les pages web où mènent ces liens n'engagent en rien la responsabilité de CIDE qui n'a pas le contrôle de ces liens.
 				L'Utilisateur s'interdit donc à engager la responsabilité du site concernant le contenu et les ressources relatives à ces liens hypertextes sortants.
-				<br/><br/></p><h3>ARTICLE 9 : Évolution du contrat</h3><p>
+				<br><br></p><h3>ARTICLE 9 : Évolution du contrat</h3><p>
 				Le site se réserve à tout moment le droit de modifier les clauses stipulées dans le présent contrat.
-				<br/><br/></p><h3>ARTICLE 10 : Durée</h3><p>
+				<br><br></p><h3>ARTICLE 10 : Durée</h3><p>
 				La durée du présent contrat est indéterminée. Le contrat produit ses effets à l'égard de l'Utilisateur à compter de l'utilisation du service.
-				<br/><br/></p><h3>ARTICLE 11 : Droit applicable et juridiction compétente</h3><p>
+				<br><br></p><h3>ARTICLE 11 : Droit applicable et juridiction compétente</h3><p>
 				Vous acceptez par la présente d’indemniser, de défendre et de protéger CIDE et ses filiales, sociétés affiliées, agents et dirigeants contre toute réclamation, poursuite juridique, dommages, pertes, responsabilités et coûts (en ce compris les honoraires d’avocat) causé par ou découlant, directement ou indirectement, de votre utilisation ou mauvais usage des services CIDE, toute violation par vous de l’une quelconque des dispositions du présent contrat ou de la politique de confidentialité, ou toute violation par vous de tout droit de tiers. CIDE se réserve le droit, à ses frais et à son entière et absolue discrétion, de décider d’assumer la défense et le contrôle de toute affaire qui pourrait faire l’objet d’une indemnisation de votre part, auquel cas vous devrez coopérer avec CIDE aux fins de fournir tout élément nécessaire à cette défense.
 				La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, seuls les tribunaux du ressort de la Cour d'appel  de la ville de Strasbourg sont compétents.
-				<br/><br/></p><h3>ARTICLE 12 : Publication par l’Utilisateur</h3><p>
+				<br><br></p><h3>ARTICLE 12 : Publication par l’Utilisateur</h3><p>
 				Le site permet aux membres de publier des commentaires / des œuvres personnelles.
 				Dans ses publications, le membre s’engage à respecter les règles de la Netiquette et les règles de droit en vigueur.
 				Le site exerce une modération a posteriori sur les publications et se réserve le droit de refuser leur mise en ligne, sans avoir à s’en justifier auprès du membre.

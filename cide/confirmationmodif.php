d3f4da55929c839d04b46b0aede17f6855dcf7c5
vincentbasset <vincent.basset@uha.fr>
2017-06-11 20:29:06 +0200

    w3c

--- cide/confirmationmodif.php
+++ cide/confirmationmodif.php
@@ -1,6 +1,12 @@
 <?php
 	include("header.php");
-	echo "<div class='col-sm-1 col-perso' id='deco'><p>Nous vous avons envoyé un nouveau mot de passe par mail</p></div>";
 ?>
+	<div class='col-sm-1 col-perso' id='deco'>
+		<p>
+			Nous vous avons envoyé votre nouveau mot de passe par mail.
+		</p>
+	</div>
+
+	</div>
 	</body>
 </html>

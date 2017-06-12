f604b8102f521787cec2cd0140adb859f9b5030e
fannyringler <fanny.ringler@uha.fr>
2017-06-12 15:16:11 +0200

    w3c validé: changermdp

--- cide/changermdp.php
+++ cide/changermdp.php
@@ -7,13 +7,13 @@
 			<p>
 				Changer de mot de passe
 				<form name="inscription" method="post" action="mdpchange.php">
-					<input type="password" name="ancienmdp" placeholder="Ancien mot de passe" required>
+					<input type="password" name="ancienmdp" placeholder="Ancien mot de passe" required />
 					<br>
 					<br>
-					<input type="password" name="mdp" placeholder="Nouveau mot de passe" required>
+					<input type="password" name="mdp" placeholder="Nouveau mot de passe" required />
 					<br>
 					<br>
-					<input type="password" name="mdp verification" placeholder="Vérifiez votre mot de passe" required>
+					<input type="password" name="mdp verification" placeholder="Vérifiez votre mot de passe" required />
 					<br>
 					<br>
 					<input id="motdp" type="submit" value="Changer de mot de passe" name="envoyer" onmouseover="javascript:check();" />

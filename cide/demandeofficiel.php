d15de0ada464afb8b49f63ac841716163a535a67
fannyringler <fanny.ringler@uha.fr>
2017-06-13 10:41:06 +0200

    w3c

--- cide/demandeofficiel.php
+++ cide/demandeofficiel.php
@@ -23,7 +23,7 @@
 	}
 	else {
 		//ce qui ce passe si on est pas co, avec un exemple de formulaire liant à un fichier qui fera le login
-		echo '<div class="col-sm-1 col-perso" id="deco"">
+		echo '<div class="col-sm-1 col-perso" id="deco">
 			<p>
 			Connecte toi ou rejoins nous !
 			</br>

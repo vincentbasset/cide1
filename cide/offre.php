029451246d70edee19bb090a0bfa94c9b3932b49
Getsuren <vernay-jacques@hotmail.fr>
2017-06-13 15:40:28 +0200

    Update offre.php

--- cide/offre.php
+++ cide/offre.php
@@ -1,9 +1,12 @@
 
+
 <?php
     include("header.php");
     if (isset($_SESSION['id'])) {
 		//ce qui se passe si on est co
 		include("offreconnecte.php");
+        echo '</div>';
+        include "chat/footer.php";
 	}
 	else {
 		//ce qui ce passe si on est pas co, avec un exemple de formulaire liant à un fichier qui fera le login
@@ -13,9 +16,8 @@
 				</br>
 				</br>
 				<a class="inscription" href="inscription.php">Pas encore inscrit ? Par ici !</a>
-				</p></div>';
+				</p></div></div>';
 	}
-	echo "</div>
-			</body>
+	echo "</body>
 		  </html>";
 ?>

0e2a2f3afba021531ed220e819a0e960e45fd771
Getsuren <vernay-jacques@hotmail.fr>
2017-06-13 15:36:58 +0200

    Create creagroupe.php

--- cide/creagroupe.php
+++ cide/creagroupe.php
@@ -1,7 +1,9 @@
 <?php
     include("header.php");
     if (isset($_SESSION['id'])){ //ce qui se passe si on est co
-		include("creagroupeconnecte.php");
+		include("creagroupeconnecte.php");   
+        	echo '</div>';
+        	include("chat/footer.php");
 	}
 	else{ //ce qui ce passe si on est pas co
 		echo"<div class='col-sm-1 col-perso' id='deco'>
@@ -11,10 +13,10 @@
 					<br>
 					<a class='inscription' href='inscription.php'>Pas encore inscrit ? Par ici !</a>
 				</p>
-			</div>";
+			</div></div>";
 	}
 ?>
 
-	</div>
+	
 	</body>
 </html>

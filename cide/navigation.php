b3d33aa07e46d6fc4f02e86f8e0c97ec016273b3
vincentbasset <vincent.basset@uha.fr>
2017-06-13 07:58:12 +0200

    ajout de l'edt

--- cide/navigation.php
+++ cide/navigation.php
@@ -1,5 +1,5 @@
 <div class="container-fluid" style="background-color:rgb(43,87,160);color:#fff;height:200px;">
-  <img src="image/logo.jpg" title="Le Cercle d'Ingénieurs de l'ENSISA" class="logo" alt="C.I.D.E."/>
+  <img src="image/logo.jpg" title="Le Cercle d'Ingénieurs de l'ENSISA" alt="C.I.D.E." width="300px" height="200px" />
 </div>
 
 <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="200">
@@ -35,6 +35,7 @@
 			?>
 		</li>
 		<li><a href="offre.php?variable=0">Offre</a></li>
+		<li><a href="http://edt.iariss.fr/" target="_blank">EDT IARISS</a></li>
       </ul>
     </div>
   </div>
